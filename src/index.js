8818d3f1613b071a37f55ddecf30c7bb2fcba24e
EDWIN GEORGE LAZER <edwin04@gmail.com>
2023-09-17 07:32:07 +0100

    React Preset

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-const $ = require('jquery')
-$("#target").html("Hello World!")
+import React from 'react'
+import { render } from 'react-dom'
+
+
+const Greet = () => <h1>Hello from React</h1>
+
+render(
+    <Greet />,
+    document.getElementById('target')
+)
